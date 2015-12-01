ee59c3ee2fbd740449e88ea7790db68f074ac118
olavjens777 <olavreitsma@gmail.com>
2015-12-01 13:20:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-
+var user = prompt("Are you a guy?").toLowerCase();
+switch(user) {
+    case 'yes':
+        if(prompt("How old are you?")>9 && prompt("Are u sure?") === true) {
+            console.log("So you are a guy who is older than 9 years old.")
+        }
+        else {
+            console.log("You can't play this game ahaha!")
+        }
+        break;
+    case 'no':
+        console.log("You are a girl. Why d you play games?")
+        break;
+    case 'maybe':
+        console.log("You smart huh?")
+        if (prompt("What is your job") === "Hacker" || "Troll") {
+            console.log("Fuck.")
+        }
+        else {
+            console.log("You are too smart for this game.")
+        }
+        break;
+    default:
+        
+}
